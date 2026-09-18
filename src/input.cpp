213e1be23c0ca60560b264863fba8f975882ca21
KHO YEE THENG <kho.yee.theng@student.mmu.edu.my>
2026-09-18 23:08:07 +0800

    Add URL input and validation

--- src/input.cpp
+++ src/input.cpp
@@ -2,3 +2,41 @@
 #include <string>
 
 using namespace std;
+
+bool validateURL(string url)
+{
+    if (url.empty())
+    {
+        return false;
+    }
+
+    if (url.find("http://") == 0)
+    {
+        return true;
+    }
+
+    if (url.find("https://") == 0)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void processURL()
+{
+    string url;
+
+    cout << "\nEnter URL: ";
+    getline(cin, url);
+
+    if (validateURL(url))
+    {
+        cout << "Valid URL.\n";
+        cout << "Content: " << url << endl;
+    }
+    else
+    {
+        cout << "Invalid URL.\n";
+    }
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,28 @@
 
 using namespace std;
 
+void processURL();
+bool validateURL(string url);
+
 int main ()
 {
-    cout<<"QR Code Content Assistant"<< endl;
+    int choice;
+
+    cout << "QR Code Content Assistant\n";
+    cout << "1. URL\n";
+    cout << "Enter your choice: ";
+
+    cin >> choice;
+    cin.ignore();
+
+    if (choice == 1)
+    {
+        processURL();
+    }
+    else
+    {
+        cout << "Invalid choice.\n";
+    }
 
     return 0;
 }
